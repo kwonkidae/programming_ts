70e68164d0e2176a2f3b137c4033c6886793077d
kkdosk <kkdosk@naver.com>
2019-09-16 23:28:38 +0900

    generic

--- chapter-4/dist/polymorphism.js
+++ chapter-4/dist/polymorphism.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const l = console.log;
 (() => {
     // type Filter = {
@@ -25,11 +26,52 @@ const l = console.log;
     l(filter(names, _ => _.firstName.startsWith("b")));
 })();
 (() => {
-    // function map(array: unknown[], f: (item: unknown) => unknown): unknown[] {
-    // }
+    function map(array, f) {
+        const result = [];
+        for (let i = 0; i < array.length; i++) {
+            result[i] = f(array[i]);
+        }
+        return result;
+    }
     const value = 42;
     const someString = value;
     console.log(someString);
-    const otherString = someString.toUpperCase();
+    // const otherString = someString.toUpperCase() runtime error
+    l(map(["a", "b", "c"], _ => _ === "a"));
+    l(map(["a", "b", "c"], _ => _ === "a"));
+    let promise = new Promise(resolve => resolve(45));
+    promise.then(result => result * 4);
+})();
+// tree
+(() => {
+    let a = { value: "a" };
+    let b = { value: "b", isLeaf: true };
+    let c = { value: "c", children: [b] };
+    let a1 = mapNode(a, _ => _.toUpperCase());
+    l(a1);
+    let b1 = mapNode(b, _ => _.toUpperCase());
+    l(b1);
+    let c1 = mapNode(c, _ => _.toUpperCase());
+    l(c1);
+    function mapNode(node, f) {
+        return {
+            ...node,
+            value: f(node.value)
+        };
+    }
+    function logPerimeter(s) {
+        console.log(s.numberOfSides * s.sideLength);
+        return s;
+    }
+    let square = { numberOfSides: 4, sideLength: 3 };
+    logPerimeter(square);
+    // model arity
+    function call(f, ...args) {
+        return f(...args);
+    }
+    function fill(length, value) {
+        return Array.from({ length }, () => value);
+    }
+    l(call(fill, 10, 5));
 })();
 //# sourceMappingURL=polymorphism.js.map
--- chapter-4/src/polymorphism.ts
+++ chapter-4/src/polymorphism.ts
@@ -1,3 +1,5 @@
+import { stringify } from "querystring";
+
 const l = console.log;
 (() => {
   // type Filter = {
@@ -42,11 +44,83 @@ const l = console.log;
 })();
 
 (() => {
-  // function map(array: unknown[], f: (item: unknown) => unknown): unknown[] {
-
-  // }
+  function map<T, U>(array: T[], f: (item: T) => U): U[] {
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      result[i] = f(array[i])
+    }
+    return result
+  }
   const value: unknown = 42;
   const someString: string = value as string;
   console.log(someString)
   // const otherString = someString.toUpperCase() runtime error
+
+  l(map(["a", "b", "c"], _ => _ === "a"))
+  l(map<string, boolean | string>(["a", "b", "c"], _ => _ === "a"))
+
+  let promise = new Promise<number>(resolve => resolve(45))
+  promise.then(result => result * 4)
+})();
+
+// tree
+(() => {
+  type TreeNode = {
+    value: string
+  }
+
+  type LeafNode = TreeNode & {
+    isLeaf: true
+  }
+
+  type InnerNode = TreeNode & {
+    children: [TreeNode] | [TreeNode, TreeNode]
+  }
+
+  let a: TreeNode = {value: "a"}
+  let b: LeafNode = {value: "b", isLeaf: true}
+  let c: InnerNode = {value: "c", children: [b]}
+
+  let a1 = mapNode(a, _ => _.toUpperCase())
+  l(a1)
+  let b1 = mapNode(b, _ => _.toUpperCase())
+  l(b1)
+  let c1 = mapNode(c, _ => _.toUpperCase())
+  l(c1)
+  function mapNode<T extends TreeNode>(
+    node: T,
+    f: (value: string) => string
+  ): T {
+    return {
+      ...node,
+      value: f(node.value)
+    }
+  }
+
+  type HasSides = { numberOfSides: number }
+  type SidesHaveLength = { sideLength: number }
+
+  function logPerimeter<Shape extends HasSides & SidesHaveLength>(s: Shape): Shape {
+    console.log(s.numberOfSides * s.sideLength)
+    return s
+  }
+
+  type Square = HasSides & SidesHaveLength
+  let square: Square = { numberOfSides: 4, sideLength: 3}
+  logPerimeter(square)
+
+  // model arity
+
+  function call<T extends unknown[], R>(
+    f: (...args: T) => R,
+    ...args: T
+  ): R {
+    return f(...args)
+  }
+
+  function fill(length: number, value: string): string[] {
+    return Array.from({length}, () => value)
+  }
+
+  l(call(fill, 10, "a"))
 })();
